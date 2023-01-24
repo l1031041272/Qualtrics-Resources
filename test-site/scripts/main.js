0344c67a56b838bbb322cd81e0eed8c385d88e84
LYU SIQI <1031041272@qq.com>
2023-01-24 15:39:08 +0900

    调试

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -76,9 +76,8 @@ function c(){
         indexLibrary = new IndexLibrary(debug, modelPath, position);
         //indexLibrary = new IndexLibrary(debug, serverURL,modelPath, position);
         indexLibrary.onload();
+        indexLibrary.App_set_point(0);
     };
-
-
 
     console.log("スクリプト読み込み");
     loadScript(0);
